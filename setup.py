616615355a6d43bad55f497d641152f58d0c1404
Matthieu Bruneaux <matthieu.bruneaux@gmail.com>
2015-09-10 11:15:11 +0300

    Bump version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@
 from setuptools import setup, find_packages
 
 setup(name = "pygenes",
-      version = "0.0.7",
+      version = "0.0.8",
       py_modules = ["pygenes", "pygenesScripts"],
       entry_points =  {
           "console_scripts" : [
